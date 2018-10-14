40cde34b65100ea04611917f7d0fbd5254269803
Till Bungert <tillbungert@gmail.com>
2018-10-14 23:28:37 +0200

    Make both scripts have the same interface

--- data/square.py
+++ data/square.py
@@ -1,7 +1,19 @@
 from PIL import Image, ImageDraw
 import numpy as np
+import fire
+import tqdm
+from pathlib import Path
 
-def generate(n_samples, imsize, sqsize=None, allow_occlusion=False):
+
+def render(n_samples, imsize, sqsize=None, allow_occlusion=False,
+           root='./squares'):
+
+    root = Path(root)
+    root.mkdir(exist_ok=True)
+
+    if not sqsize:
+        sqsize = np.random.random_sample() * (0.5 - 0.1) * imsize + 0.1*imsize
+
     # Choose rectangle coordinates in allowed area
     if allow_occlusion:
         max_xy = imsize
@@ -10,10 +22,10 @@ def generate(n_samples, imsize, sqsize=None, allow_occlusion=False):
         max_xy = imsize - np.sqrt(2)*sqsize/2
         min_xy = np.sqrt(2)*sqsize/2
 
-    for i in range(n_samples):
+    for i in tqdm.trange(n_samples, dynamic_ncols=True):
         x0 = np.random.randint(min_xy, max_xy, 2)
-        rect_xy = np.array([[     0,      0],
-                            [     0, sqsize],
+        rect_xy = np.array([[0,           0],
+                            [0,      sqsize],
                             [sqsize, sqsize],
                             [sqsize,      0],
                             ]) + x0 - sqsize/2
@@ -29,8 +41,8 @@ def generate(n_samples, imsize, sqsize=None, allow_occlusion=False):
         img = Image.new('RGB', (imsize, imsize), 0)
         draw = ImageDraw.Draw(img)
         draw.polygon(list(rect_xy.flatten()), fill=(255, 255, 255))
-        img.save(f'square/{i:05d}.png')
+        img.save(root / f'{i:05d}.png')
 
 
 if __name__ == '__main__':
-    generate(10, 512, 100)
+    fire.Fire(render)
