79946034b7aeb842a8b8b5085146c7f0a23366a2
Till Bungert <tillbungert@gmail.com>
2019-01-15 11:03:09 +0100

    Add vsd, not yet functional

--- eval.py
+++ eval.py
@@ -2,15 +2,42 @@ import torch
 
 from data_loader import get_loader
 
-from utils import Codebook, symmetries, symmetries_diff
+from utils import Codebook, symmetries, symmetries_diff, eulerAnglesToRotationMatrix
 import argparse
 from tqdm import tqdm
-import matplotlib.pyplot as plt
+import numpy as np
+import trimesh
+
+from sixd_toolkit.pysixd import pose_error
 
 if torch.cuda.is_available():
     device = 'cuda'
 else:
     device = 'cpu'
+
+
+def vsd(all_pose_est, all_pose_gt):
+    K = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    model = trimesh.load("./data/cat/mesh.ply")
+    vert = model.vertices - model.center_mass
+    vert /= np.abs(vert).max()
+    vert *= 2
+    model = {"pts": vert, "faces": model.faces}
+    errs = []
+    i = 0
+    for est, gt in tqdm(zip(all_pose_est, all_pose_gt)):
+        if i > 50:
+            break
+        errs.append(pose_error.vsd(*est, *gt, model, 100*np.ones((128, 128)), K,
+                                   15, 30))
+        i += 1
+
+    errs = np.array(errs)
+
+    print("VSD:")
+    print(errs)
+    print(errs.mean())
+    print(errs.max())
 
 
 if __name__ == "__main__":
@@ -37,7 +64,7 @@ if __name__ == "__main__":
     print(codebook.rot.shape)
 
     loader_test = get_loader(f'./data/{args.dataset}', image_size=128,
-                             batch_size=64, dataset='Geometric',
+                             batch_size=1, dataset='Geometric',
                              mode='test', num_workers=4,
                              pin_memory=True, mean=[0]*3,
                              std=[1]*3)
@@ -45,6 +72,9 @@ if __name__ == "__main__":
     length = len(loader_test)
     MSE_trans = torch.zeros(3).to(device)
     MSE_rot = torch.zeros(3).to(device)
+    all_errors = []
+    all_pose_est = []
+    all_pose_gt = []
 
     for i, (x, _1, _2, label) in tqdm(enumerate(loader_test)):
         x = x.to(device)
@@ -61,6 +91,11 @@ if __name__ == "__main__":
         # print('trans',trans[:20])
         rot = symmetries(rot, object_type=args.dataset)
         rot_ = symmetries(rot_, object_type=args.dataset)
+
+        all_pose_est.append((eulerAnglesToRotationMatrix(rot_),
+                             trans_.cpu()))
+        all_pose_gt.append((eulerAnglesToRotationMatrix(rot[0]),
+                            trans.cpu()))
         with torch.no_grad():
             abs_diff = symmetries_diff(rot, rot_, object_type=args.dataset)
             print(abs_diff.max())
@@ -69,6 +104,3 @@ if __name__ == "__main__":
 
     print(torch.sqrt(MSE_trans))
     print(torch.sqrt(MSE_rot))
-
-plt.scatter(codebook.z_rot[:, 0].cpu().numpy(), codebook.rot[:, 0].cpu().numpy())
-plt.show()
